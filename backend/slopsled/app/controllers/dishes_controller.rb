faa0b7f55b11949e27ce9df99c0d67d6eefdacd9
satodd1967 <seth.todd@outlook.com>
2021-01-13 10:59:26 -0600

    Added the JSON serializer renders for the dishes controller, but deleted the category option in the dish serializer because there is no actual relationship in the database.  May go back and add a belongs to in the dishes table, but may not.

--- backend/slopsled/app/controllers/dishes_controller.rb
+++ backend/slopsled/app/controllers/dishes_controller.rb
@@ -3,14 +3,16 @@ class DishesController < ApplicationController
 
   # GET /dishes
   def index
-    @dishes = Dish.all
-
-    render json: @dishes
+    # @dishes = Dish.all
+    # render json: @dishes
+    dishes = Dish.all
+    render json: DishSerializer.new(dishes)
   end
 
   # GET /dishes/1
   def show
-    render json: @dish
+    # render json: @dish
+    render json: DishSerializer.new(@dish)
   end
 
   # POST /dishes
