3c56cdf3a91d6070ccef1013df05e118c79149ed
satodd1967 <seth.todd@outlook.com>
2021-01-13 20:00:12 -0600

    Added category.js to the src folder, added customer.js to the source folder, added dish.js to the source folder, added line_item.js to the source folder, added order.js to the source folder, added restaurant.js to the source folder.  Added an event listern to the top of index.js, added a BASE URL constant to index.js, added a function fetchCategories to the index.js file.  Added a renderCategory function to the category.js file.  Added a bunch of additional commented out code from a previous project as reference in index.js.

---
+++ frontend/src/category.js
@@ -0,0 +1,20 @@
+class Category{
+    constructor(id, name, description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    renderCategory() {
+        let categoriesDiv = document.getElementById("categories-container")
+
+        categoriesDiv.innerHTML +=
+        `
+        <ul>
+        <h3>Name: ${this.name}</h3>
+        <li>description: ${this.description}</li>
+        </ul>
+        <button class="delete-bttn" data-id=${this.id} onclick="deleteUser()">Delete User</button>
+        `
+    }
+}
