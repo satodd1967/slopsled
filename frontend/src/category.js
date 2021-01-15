837979f35afddc79668af0c78a549c1cff98c867
satodd1967 <seth.todd@outlook.com>
2021-01-14 21:45:24 -0600

    added a dataID field to the restaurant choose button that holds the category ID.  This should be used to render the restaruants in the fetchRestaurantsByCat function

--- frontend/src/category.js
+++ frontend/src/category.js
@@ -14,7 +14,7 @@ class Category{
         <h3>Name: ${this.name}</h3>
         <li>description: ${this.description}</li>
         </ul>
-        <button class="choose-category-btn" onclick="openRestaurants()">Choose</button>
+        <button class="choose-category-btn" dataId="${this.id} onclick="openRestaurants()">Choose</button>
         `
     }
 }
