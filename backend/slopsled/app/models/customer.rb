4b3f0e65ef8619e41f5f62d4bd48e7f6535cf019
satodd1967 <seth.todd@outlook.com>
2021-02-06 16:52:43 -0600

    Added a function called checkDelete to the customer class in the frontend.  This function checks to see if the username is black in the workingCustomer array.  If it is blank then the function sends a fetch request to delete the customer and by dependent deleting all of that customers associated orders and line items.  Also added an event listener on beforeunload that fires this function.  This basically keeps the database clean.  A new order is created everytime someone hits the page and also a new customer, but they are both blank. Before unload the page checks to see if these are still blank if so then it deletes them and assocaited data from the database.

--- backend/slopsled/app/models/customer.rb
+++ backend/slopsled/app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-    has_many :orders
+    has_many :orders, dependent: :destroy
     has_many :line_items, through: :orders
 end
--- backend/slopsled/app/models/order.rb
+++ backend/slopsled/app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   has_many :dishes, through: :line_items
 end
 
